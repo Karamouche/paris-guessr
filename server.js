fc3cc0c67b41ab383931d93cc912214b1e0ebe30
Karamouche <hugo4ibt@gmail.com>
2023-04-02 23:27:00 +0200

    update port info

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
