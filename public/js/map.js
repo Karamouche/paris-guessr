63dbce89ec5c5c2c9b58e73a45714a2bb2bc4a49
Karamouche <hugo4ibt@gmail.com>
2023-04-03 11:41:36 +0200

    Map interaction and marker coords sending

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,26 @@
 const initMap = () => {
-	const paris = {lat: 48.856614, lng: 2.3522219};
-	const zoom = 12;
-
-	const map = L.map("mapmain").setView([paris.lat, paris.lng], zoom);
+	const map = L.map("mapmain");
+	map.center = [48.856614, 2.3522219]; //Paris center location
+	map.zoom = 12;
+	map.setView(map.center, map.zoom);
+	//set max bound of map to paris area only with a margin
+	map.setMaxBounds([
+		[48.75, 2.15],
+		[48.95, 2.55]
+	]);
 	const mainLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		minZoom: zoom,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		minZoom: map.zoom,
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	});
 	mainLayer.addTo(map);
+
+	const mapContainer = $('#mapcontainer');
+
+	//observer to detect map container resize
+	const resizeObserver = new ResizeObserver(() => {
+		map.invalidateSize();
+	});
+	resizeObserver.observe(mapContainer[0]);
 	return map;
 }
 
@@ -17,12 +30,36 @@ const onMapClick = ({map, e}) => {
 			map.removeLayer(layer);
 		}
 	});
-	const marker = L.marker(e.latlng).addTo(map);
+	L.marker(e.latlng).addTo(map);
+}
+
+const onGuessClick = ({map, e}) => {
+	let marker;
+	map.eachLayer((layer) => {
+		if (layer instanceof L.Marker) {
+			marker = layer;
+		}
+	});
+	if (marker) {
+		console.log(marker.getLatLng());
+	}
 }
 
 const main = () => {
 	const map = initMap();
 	map.on('click', (e) => onMapClick({map, e}));
+	map.on('keypress', (e) => {
+		if (e.originalEvent.key === 'r') {
+			map.eachLayer((layer) => {
+				if (layer instanceof L.Marker) {
+					map.setView(layer.getLatLng());
+				}else{
+					map.setView(map.center);
+				}
+			});
+		}
+	});
+	$('#guessbutton').on('click', (e) => onGuessClick({map, e}));
 };
 
 main();
